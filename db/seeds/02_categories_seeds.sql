23d8822d6724f3962844ca13d5165cef2ec407ff
Michael Hallett <m.hallett@gmail.com>
2020-05-30 15:11:57 +0000

    Added development seed data

---
+++ db/seeds/02_categories_seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories (name) Values (film_show);
+INSERT INTO categories (name) Values (restaurant);
+INSERT INTO categories (name) Values (product);
+INSERT INTO categories (name) Values (book);
+INSERT INTO categories (name) Values (uncat);
+INSERT INTO categories (name) Values (completed);
+
