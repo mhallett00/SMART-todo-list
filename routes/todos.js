290b66817158890c82ea712ba0b4e859243c5e86
Michael Hallett <m.hallett@gmail.com>
2020-06-01 19:37:07 +0000

    Added async and proper db query handling to routes. query logic mvoed to dbhelpers.js

--- routes/todos.js
+++ routes/todos.js
@@ -1,54 +1,32 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (db) => {
+module.exports = (dbhelpers) => {
+
   router.get("/", (req, res) => {
-    let query = `
-    SELECT to_dos.*
-    FROM to_dos
-    JOIN users ON user_id = users.id
-    WHERE users.name = '${req.session.user_id}'
-    `;
-    db.query(query)
-    .then(data => {
-      const to_dos = data.rows;
-      res.json({ to_dos })
-    })
+    dbhelpers.getTodos(req.session.user_id)
+    .then(todos => res.json({ todos }))
     .catch(err => {
       res
         .status(500)
         .json( { error: err.message });
     })
-  })
-  return router;
-}
+  });
 
-
-
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
-/* const express = require('express');
-const router  = express.Router();
-
-module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
+  router.post('/', async (req, res) => {
+    const userId = await dbhelpers.getIdWithUsername(req.session.user_id);
+    console.log({...req.body, user_id: userId})
+    dbhelpers.addTodo({...req.body, user_id: userId})
+      .then(todo => {
+        res.send(todo);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch(e => {
+        console.error(e);
+        res.send(e)
       });
   });
+
   return router;
-}; */
+};
 
 
