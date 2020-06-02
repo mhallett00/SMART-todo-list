c838c07025b35648faf6249b9f1e48e6316bde47
Michael Hallett <m.hallett@gmail.com>
2020-06-02 19:23:39 +0000

    Added pseudo & test code

--- routes/todos.js
+++ routes/todos.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const { apiBook } = require('./api');
 
 module.exports = (dbhelpers) => {
 
@@ -13,9 +14,18 @@ module.exports = (dbhelpers) => {
     })
   });
 
+  // posting new item to database
+  // 1. converting username to userID (line 23)
+  // 2. ping API and RETURN category_id based on result
+  //   a. if (req.body.length > 0) {RETURN x}
+  // 3. take userID, catID, req.body -> use SQL to insert into databse
+
+  // adds item when form is submitted
   router.post('/', async (req, res) => {
     const userId = await dbhelpers.getIdWithUsername(req.session.user_id);
-    console.log({...req.body, user_id: userId})
+    // console.log({...req.body, user_id: userId})
+    // const categoryId = await apiBook(callback, req.body)
+    // console.log(categoryId);
     dbhelpers.addTodo({...req.body, user_id: userId})
       .then(todo => {
         res.send(todo);
