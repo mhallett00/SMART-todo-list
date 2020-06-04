f1a7b89d04e65cf5eab69687f7c5e389a795032a
Michael Hallett <m.hallett@gmail.com>
2020-06-04 21:28:51 +0000

    Added delete handling

--- public/scripts/to_dos.js
+++ public/scripts/to_dos.js
@@ -1,4 +1,5 @@
 $(() => {
+
 const loadTodos = function() {
   $.ajax( { url: '/api/to_dos', method: 'GET' } )
     .done(function(result) {
@@ -11,19 +12,19 @@ const loadTodos = function() {
 const renderTodos = function(todos) {
 
   for (let todo of todos) {
-    if (todo.category_id === 1){
+    if (todo.category_id === 1 && todo.status_id !== 3){
       $('.film_shows').prepend(createTodo(todo));
     };
-    if (todo.category_id === 2) {
+    if (todo.category_id === 2 && todo.status_id !== 3) {
       $('.restaurants').prepend(createTodo(todo));
     };
-    if (todo.category_id === 3) {
+    if (todo.category_id === 3 && todo.status_id !== 3) {
       $('.products').prepend(createTodo(todo));
     };
-    if (todo.category_id === 4){
+    if (todo.category_id === 4 && todo.status_id !== 3){
       $('.books').prepend(createTodo(todo));
     };
-    if (todo.category_id === 5) {
+    if (todo.category_id === 5 && todo.status_id !== 3) {
       $('.uncategorized').prepend(createTodo(todo));
     };
     $(`#item-${todo.id}`).click(()=>{
@@ -36,6 +37,7 @@ const renderTodos = function(todos) {
   };
 
 };
+
 // somehow we need to tie modal show with this modal
     $("input[type='radio']").click(()=>{
       const category_id = Number($("input[name='optradio']:checked").val());
@@ -45,9 +47,21 @@ const renderTodos = function(todos) {
       .done(function(result) {
         $('#myModal').modal('hide');
         $("input[type='radio']").removeAttr("id");
-        $(`#item-${id}`).remove();
+        $(`#divCheck${id}`).remove();
         renderTodos(result);
 
+      })
+    });
+
+    $("#tododelete").click(()=>{
+      const status_id = 3;
+      const id = Number($("input[name='optradio']:checked").attr("id"));
+      console.log('hello', typeof status_id, status_id);
+      $.ajax({ url: '/api/to_dos/delete', method: 'POST', data: {status_id, id} })
+      .done(function(result) {
+        $('#myModal').modal('hide');
+        $("input[type='radio']").removeAttr("id");
+        $(`#divCheck${id}`).remove();
       })
     });
 
@@ -56,42 +70,33 @@ $('#newToDo').submit(function(event) {
   event.preventDefault();
   const name = this.elements.name.value
   console.log('submission', name)
-  $.ajax( { url: '/api/to_dos', method: 'POST', data: { name } })
-    .done(function(result) {
-      // console.log(result)
-      renderTodos(result);
-    })
+  if (name) {
+    $.ajax( { url: '/api/to_dos', method: 'POST', data: { name } })
+      .done(function(result) {
+        // console.log(result)
+        renderTodos(result);
+      })
+  } else {
+    alert("Surely you'd like to add an item?");
+  }
 });
 
 // creates an html snippet to wrap a new todo for display
 const createTodo = function(todo) {
   console.log(todo)
-  const $todo = $(`<p id='item-${todo.id}' >${todo.name}</p>`);
+  const $todo = $( `
+  <div class="form-check" id="divCheck${todo.id}">
+  <input class="form-check-input" type="checkbox" value="" id="defaultCheck${todo.id}">
+  <label class="form-check-label" for="defaultCheck1" id='item-${todo.id}'>
+  ${todo.name}
+  </label>
+  </div>`);
+
   return $todo;
 };
 
-//brings up edit menu
-// $("p").click(function(event) {
-  // event.preventdefault();
-  // const name = this.element.name.value
-  // console.log(this);
-  // alert(event);
-  // $.ajax( {url: '/api/to_dos/edit', mehtod: 'GET', data: {name} })
-  // .done(function(result) {
-
-  // })
-// })
-
-//posts to do edit
-// $('').click(function(event) {
-//   event.preventdefault();
-//   const name = this.element.name.value
-//   $.ajax( {url: '/api/to_dos/edit', method: 'POST', data: {name} })
-//   .done(function(result) {
-//     updateTodo(catid, getTodoInfo(name) )
-//   })
-// })
-
+{/* <p id='item-${todo.id}' >${todo.name}</p>`
+ */}
 loadTodos();
 
 
