881f5e88ca34192756d5576446b34149d06f3050
Michael Hallett <m.hallett@gmail.com>
2020-06-04 18:05:56 +0000

    Added modal to handle editing todo categories

--- public/scripts/to_dos.js
+++ public/scripts/to_dos.js
@@ -2,7 +2,7 @@ $(() => {
 const loadTodos = function() {
   $.ajax( { url: '/api/to_dos', method: 'GET' } )
     .done(function(result) {
-      console.log("hello", result);
+      // console.log("hello", result);
       renderTodos(result.todos);
     })
 };
@@ -12,36 +12,53 @@ const renderTodos = function(todos) {
 
   for (let todo of todos) {
     if (todo.category_id === 1){
-      $('#film_shows').append(createTodo(todo));
+      $('.film_shows').prepend(createTodo(todo));
     };
     if (todo.category_id === 2) {
-      $('#restaurants').append(createTodo(todo));
+      $('.restaurants').prepend(createTodo(todo));
     };
     if (todo.category_id === 3) {
-      $('#products').append(createTodo(todo));
+      $('.products').prepend(createTodo(todo));
     };
     if (todo.category_id === 4){
-      $('#books').append(createTodo(todo));
+      $('.books').prepend(createTodo(todo));
     };
     if (todo.category_id === 5) {
-      $('#uncategorized').append(createTodo(todo));
+      $('.uncategorized').prepend(createTodo(todo));
     };
     $(`#item-${todo.id}`).click(()=>{
-      $('#myModal').modal('show');
+      $('#myModal').modal();
+      console.log(todo.id);
+      $("input[type='radio']").attr("id", `${todo.id}`);
+      // add temporary html tag of todo.id
       // alert(JSON.parse(JSON.stringify(todo)))
     })
   };
 
 };
+// somehow we need to tie modal show with this modal
+    $("input[type='radio']").click(()=>{
+      const category_id = Number($("input[name='optradio']:checked").val());
+      const id = Number($("input[name='optradio']:checked").attr("id"));
+      // console.log('hello', radioCatId, radioItemId);
+      $.ajax({ url: '/api/to_dos/edit', method: 'POST', data: {category_id, id} })
+      .done(function(result) {
+        $('#myModal').modal('hide');
+        $("input[type='radio']").removeAttr("id");
+        $(`#item-${id}`).remove();
+        renderTodos(result);
 
+      })
+    });
 
 
 $('#newToDo').submit(function(event) {
   event.preventDefault();
   const name = this.elements.name.value
+  console.log('submission', name)
   $.ajax( { url: '/api/to_dos', method: 'POST', data: { name } })
     .done(function(result) {
-      console.log(result)
+      // console.log(result)
       renderTodos(result);
     })
 });
